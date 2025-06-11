a708aca8c37e2e3a3c335964c9b7861c5a49f17d
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2025-06-11 19:29:32 -0300

    point criado com métodos

--- point.py
+++ point.py
@@ -1,3 +1,5 @@
+import math
+
 class Point:
     """
     Representa um ponto em um espaço tridimensional.
@@ -16,5 +18,17 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
     
-    # Implemente os métodos de pontos aqui
-    
+    def __repr__(self):
+        return f"Point({self.x}, {self.y}, {self.z})"
+
+    def soma_pontos(self, outro):
+        return Point(self.x + outro.x, self.y + outro.y, self.z + outro.z)
+
+    def subtrai_pontos(self, outro):
+        return Point(self.x - outro.x, self.y - outro.y, self.z - outro.z)
+
+    def distancia(self, outro):
+        return math.sqrt((self.x - outro.x) ** 2 + (self.y - outro.y) ** 2 + (self.z - outro.z) ** 2)
+
+    def produto_escalar(self, escalar: float):
+        return Point(self.x * escalar, self.y * escalar, self.z * escalar)
